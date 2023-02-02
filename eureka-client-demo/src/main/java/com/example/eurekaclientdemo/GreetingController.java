b3a26c0d9b922af6896fdf7f59e5b1cba993382d
lruihan <lsj.ruihan@gmail.com>
2023-02-02 14:24:40 +0000

    Eureka
    1. Server
    2. Client
    3. Another Client (w/ Feign)

---
+++ eureka-client-demo/src/main/java/com/example/eurekaclientdemo/GreetingController.java
@@ -0,0 +1,8 @@
+package com.example.eurekaclientdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+    @RequestMapping("/greeting")
+    String greeting();
+}
---
+++ eureka-feign-demo/src/test/java/com/example/eurekafeigndemo/EurekaFeignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekafeigndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaFeignDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
